26c04d3e7d56046903dd75122354b714b1dc9191
Cosmos <iscosmos@outlook.com>
2023-03-22 19:24:08 +0300

    minor changes

---
+++ src/views/AboutView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import navbar from '@/components/navbar.vue';
+</script>
+
+<template>
+  <navbar />
+  <main>
+    <h1>About</h1>
+    <p>
+      This is an example project to show how to use Vue 3 with TypeScript and
+      Vite.
+    </p>
+    <p>
+      <a href="https://v3.vuejs.org/" target="_blank" rel="noopener">Vue 3 docs</a> |
+      <a href="https://vitejs.dev/guide/" target="_blank" rel="noopener">Vite docs</a>
+    </p>
+    
+    <h2>Features</h2>
+    <ul>
+      <li>Vue 3</li>
+      <li>TypeScript</li>
+      <li>Vite</li>
+      <li>Vue Router (this page)</li>
+      <li>VueX (state management)</li>
+      <li>Composition API</li>
+      <li>Single File Components</li>
+      <li>ESLint</li>
+      <li>Prettier</li>
+    </ul>
+        
+  </main>
+</template>
