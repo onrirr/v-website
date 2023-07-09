683fcd3ed853e7133816fd6cacada857d1c60995
onrir <iscosmos@outlook.com>
2023-07-09 12:00:52 +0000

    remake init

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+<footer class="mt-auto mb-0 w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
+  <!-- Grid -->
+  <div class="text-center">
+    <div>
+      <a class="flex-none text-xl font-semibold text-black dark:text-white" href="#" aria-label="Brand">V-lang</a>
+    </div>
+    <!-- End Col -->
+
+    <div class="mt-3">
+      <p class="text-gray-500">Website made by <a href="https://onrir.me">Onrir</a> for V-Lang. No rights reserved</p>
+    </div>
+
+  </div>
+</footer>
+</template>
